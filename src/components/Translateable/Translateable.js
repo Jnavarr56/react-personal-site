ac6fd82d647f35a41aee8eef1e8c96d6882691a2
Jorge Navarro <jnavarr56@gmail.com>
2019-08-07 18:43:07 -0400

    Updating package.json

--- src/components/Translateable/Translateable.js
+++ src/components/Translateable/Translateable.js
@@ -1,70 +1,55 @@
-import React from 'react'
+import React from 'react';
 
 export default class Translateable extends React.Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            fadingOut: false,
-            fadingIn: true,
-            text: ''
-        }
+    this.state = {
+      fadingOut: false,
+      fadingIn: true,
+      text: ''
+    };
 
-        this.fadeClasses = {
-            fadingOut: 'opacity-0 blur-10',
-            fadingIn:  'opacity-100 blur-0',
-        }
+    this.fadeClasses = {
+      fadingOut: 'opacity-0 blur-10',
+      fadingIn: 'opacity-100 blur-0'
+    };
+  }
+
+  componentDidMount = () => {
+    this.setState({ text: this.props.text });
+  };
+
+  componentDidUpdate = prevProps => {
+    if (prevProps.text !== this.props.text) {
+      this.setState({ fadingOut: true, fadingIn: false }, () => {
+        setTimeout(() => {
+          this.setState({ text: this.props.text }, () => {
+            setTimeout(() => {
+              this.setState({
+                fadingOut: false,
+                fadingIn: true
+              });
+            }, 250);
+          });
+        }, 250);
+      });
+    }
+  };
+
+  render = () => {
+    const { text, fadingIn, fadingOut } = this.state;
+
+    let style = '';
+
+    if (fadingIn) {
+      style = this.fadeClasses.fadingIn;
+    } else if (fadingOut) {
+      style = this.fadeClasses.fadingOut;
     }
 
-    
-    componentDidMount = () => {
-        
-        this.setState({ text: this.props.text })
-    }
-
-    componentDidUpdate = prevProps => {
-
-        if (prevProps.text !== this.props.text) {
-            
-            this.setState({ fadingOut: true, fadingIn: false }, () => {
-
-                setTimeout(() => {
-
-                    this.setState({ text: this.props.text }, () => {
-
-                        setTimeout(() => {
-    
-                            this.setState({
-                                fadingOut: false,
-                                fadingIn: true
-                            })
-    
-                        }, 250)
-    
-                    })
-
-                }, 250)
-
-            })
-
-        }
-
-    }
-
-    render = () => {
-
-        const { text, fadingIn,  fadingOut } = this.state
-
-        let style = ''
-
-        if (fadingIn) {
-            style = this.fadeClasses.fadingIn
-        }
-        else if (fadingOut) {
-            style = this.fadeClasses.fadingOut
-        }
-        
-
-        return <span className={`font-primary transition-all-50 ${style}`}>{text}</span>
-    }
+    return (
+      <span className={`font-primary transition-all-50 ${style}`}>{text}</span>
+    );
+  };
 }
