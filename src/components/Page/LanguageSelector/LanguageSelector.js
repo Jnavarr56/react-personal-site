027179b07eb2cd108fd2f1b5340f16ce1385040f
Jorge Navarro <jnavarr56@gmail.com>
2019-08-10 17:28:12 -0400

    Stopping for first refactor.

--- src/components/Page/LanguageSelector/LanguageSelector.js
+++ src/components/Page/LanguageSelector/LanguageSelector.js
@@ -6,11 +6,13 @@ import './LanguageSelector.css'
 
 const LanguageSelector = props => {
   return (
-    <div className="fixed top-0 right-0 z-999999 flex justify-center items-center pt-5 pr-5 sm:pt-16 sm:pr-24">
-      <span className="font-primary mr-2 sm:text-xl">
-        <Translateable text={props.language} />
-      </span>
-      <Toggle icons={false} onChange={props.toggle} />
+    <div className="fixed top-0 right-0 z-999999 flex justify-center items-center pt-5 pr-5 sm:pt-12 sm:pr-24">
+      <div className="bg-black rounded-full py-1 px-2 flex justify-center items-center">
+        <span className="font-primary text-white mr-2 sm:text-xl">
+          <Translateable text={props.language} />
+        </span>
+        <Toggle icons={false} onChange={props.toggle} />
+      </div>
     </div>
   )
 }
